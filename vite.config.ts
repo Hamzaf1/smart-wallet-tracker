b1fa2ce76c31703dea919513926ed0dd4f756fa6
hamzafa <elfatehhamza@gmail.com>
2026-04-26 19:01:02 +0000

    refactor: update alias resolution to use node:url in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import path from "path"
+import { fileURLToPath, URL } from "node:url"
 
 export default defineConfig({
   plugins: [
@@ -18,7 +18,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 })
